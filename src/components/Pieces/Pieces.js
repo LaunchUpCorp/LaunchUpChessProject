c978101d6a205aa964226134b0ced098931a6adb
Josh D <josh.date@techguilds.com>
2022-10-31 20:58:48 -0400

    feat: first commit

---
+++ src/components/Pieces/Pieces.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Pawn from "./ui/Pawn";
+
+const Pieces = () => {
+  return (
+    <div>
+      <div>
+        <Pawn />
+      </div>
+    </div>
+  );
+};
+
+export default Pieces;
---
+++ src/components/Pieces/ui/Pawn/Pawn.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import { ReactComponent as PawnImage } from "../../../../assets/images/pawn-image.png";
+
+const Pawn = () => {
+  return <div>{/* <PawnImage /> */}</div>;
+};
+
+export default Pawn;

e64faafd0b56f9f5625dc602f9b7bdb1859b7155
neli-rashkova <neli.rashkova73@gmail.com>
2022-11-01 18:55:19 -0400

    feat: Created landing page component with two button components in it. Title of the game is Master Chess and there is a play button and a rules button. no functionality added yet.

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "../resuableComponents/Button";
+
+
+function LandingPage() {
+    return (
+        <div className="mx-auto flex flex-col justify-start space-y-48 bg-[url('./assets/images/chessboardBg.png')] w-full h-screen bg-cover bg-no-repeat bg-center">
+            <div className="mx-auto flex flex-col items-center justify-center">
+                <h1 className="text-zinc-100 text-4xl font-roboto font-extrabold mt-28 sm:text-5xl lg:text-7xl ">MASTER GAME</h1>
+            </div>
+            <div className="mx-auto flex flex-col items-center">
+                <Button buttontext="PLAY" />
+                <Button buttontext="RULES" />
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;

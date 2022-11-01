c23eefe70d26244a4fce1df90d4611340b8bc387
neli-rashkova <neli.rashkova73@gmail.com>
2022-10-31 23:00:43 -0400

    created components folder and created ChessBoard.js component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ChessBoard from "./components/ChessBoard";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <ChessBoard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ChessBoard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ChessBoard() {
+    return (
+        <div className="border-box mx-auto w-full h-screen flex border-2 border-black">
+            <div className="">
+
+            </div>
+        </div>
+    );
+}
+
+export default ChessBoard;

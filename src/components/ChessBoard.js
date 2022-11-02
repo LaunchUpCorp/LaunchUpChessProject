0fd117adca905ddf1318b60a49d5ee53ca0978e2
Josh D <josh.date@techguilds.com>
2022-11-01 20:42:42 -0400

    feat: first commit

--- src/components/ChessBoard.js
+++ src/components/ChessBoard.js
@@ -1,11 +1,10 @@
 import React from "react";
-import {useState} from 'react';
-
+import Piece from "./Piece";
 
 function ChessBoard() {
- const handleClick = (rows,col) =>{
-    console.log(rows,col)
-  }
+  const handleClick = (rows, col) => {
+    console.log(rows, col);
+  };
 
   let arr = [];
   for (let rows = 0; rows < 8; rows++) {
@@ -13,19 +12,41 @@ function ChessBoard() {
     for (let col = 0; col < 8; col++) {
       if ((rows + col) % 2 === 0) {
         temp.push(
-          <div onClick={() =>{handleClick(rows,col)}} className="w-[12.5%] h-[12.5%] border-2 border-black bg-amber-600"></div>
+          <div
+            onClick={() => {
+              handleClick(rows, col);
+            }}
+            className="w-[12.5%] h-[12.5%] border-2 border-black bg-amber-600"
+          ></div>
         );
       } else {
         temp.push(
-          <div onClick={() =>{handleClick(rows,col)}}className="w-[12.5%] h-[12.5%] border-2 border-black bg-green-600"></div>
+          <div
+            onClick={() => {
+              handleClick(rows, col);
+            }}
+            className="w-[12.5%] h-[12.5%] border-2 border-black bg-green-600"
+          ></div>
         );
       }
     }
     arr.push(temp);
   }
 
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if ((i + j) % 2 === 0) {
+        arr[i][j] = (
+          <div className="w-[12.5%] h-[12.5%] border-2 border-black bg-amber-600 mx-auto">
+            <Piece piece={"pawn"} />
+          </div>
+        );
+      }
+    }
+  }
+
   return (
-    <div className="border-box mx-auto w-full h-screen flex items-center">
+    <div className="flex items-center w-full h-screen mx-auto border-box">
       <div className="mx-auto flex justify-center items-center flex-wrap border-2 w-[500px] h-[500px] border-slate-900">
         {arr}
       </div>
